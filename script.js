60a4c86ef9fe55ded0c0857218b9cf18942471aa
Ian <ian.xk9@gmail.com>
2023-04-11 11:19:54 -0300

    DO WHILE

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 //WHILE
 
-let cont = 0
+let cont =0
 while(cont < 10){
     document.write(" O valor é: " + cont);
     document.write("<br>")
     cont ++;
 }
+
+//DO WHILE
+
+let cont1 =0
+
+do{
+    document.write("<br><br>")
+    document.write("O valor while é: " +cont1)
+    document.write("<br>")
+    cont1++
+}while(cont1 ==0)
